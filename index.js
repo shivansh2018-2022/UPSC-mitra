313e38d059e6a14db0800850c1cb592169731d4d
shivansh verma <49519633+shivansh2018-2022@users.noreply.github.com>
2023-10-02 15:19:05 +0530

    changed helmet dependency

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      connectSrc: ["'self'", ''],
+      // Add other directives as needed
+    },
+  })
+);
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
